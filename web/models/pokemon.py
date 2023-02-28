90a6352e008051c245553f6016702a05f01db2ca
Roney Santos <joseroneysilvasantos@gmail.com>
2023-02-28 10:53:17 -0800

    Basic to running Pokedex

---
+++ web/models/pokemon.py
@@ -0,0 +1,5 @@
+class PokemonClass:
+    def __init__(self,name,number,photo):
+        self.name=name
+        self.number=number
+        self.photo=photo
--- web/pokedex.py
+++ web/pokedex.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask.globals import *
+from models.pokemon import PokemonClass
+from flask import Flask, render_template, jsonify
+import requests
+import json
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search_page", methods = ["GET", "post"])
+def buscar():
+    pokemon = PokemonClass(request.form["name_pokemon"].lower(),"","")
+    try:
+        res = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.name}").text)
+        result_number = res["id"]
+        pokemon.number = result_number
+        # Get photo
+        result_photo = res["sprites"]
+        result_photo = result_photo["front_default"]
+        pokemon.photo = result_photo
+    except:
+        return "Pokemon not found"
+    return render_template("index.html",
+    name = pokemon.name,
+    photo = pokemon.photo,
+    number = pokemon.number
+    
+    )
 
 if __name__=="__main__":
     app.run(debug=True)
